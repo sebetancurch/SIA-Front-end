03109d705548a1d803c6b0fb2d7b90dc76398637
unknown <sebetancurch@unal.edu.co>
2024-03-18 12:19:20 -0500

    remove .env xd

---
+++ src/components/Contexts/LoadingContext.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { createContext, useState } from "react";
+import { Session } from "@/types/user";
+
+export const LoadingContext = createContext({
+  loading: false,
+  setLoading: (value: boolean) => {},
+});
+
+export const LoggedUserProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function setLoading(value: boolean) {
+    setIsLoading(value);
+  }
+
+  return (
+    <LoadingContext.Provider value={{ loading: isLoading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
